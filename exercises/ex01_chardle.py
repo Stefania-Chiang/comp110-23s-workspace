63c7bf2fcec0e43f0b0c8f555b911e208f81001e
Stefania Chiang <tinge@ad.unc.edu>
2023-01-31 00:01:05 -0500

    y

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -4,5 +4,7 @@ __author__ = "730517776"
 five_character_word: str = input('Enter a 5-character word: ')
 single_character: str = input('Enter a single character: ')
 print("Searching for " + single_character + " in " + five_character_word)
-print(single_character + " found at index " + single_character)
+print(single_character + " found at index " + "five_character_word"[single_character])
+
+
 
