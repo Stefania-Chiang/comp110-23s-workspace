5ba45494102337d17b144f46855cb704bf134a85
Stefania Chiang <tinge@ad.unc.edu>
2023-01-31 00:47:07 -0500

    h

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -15,3 +15,16 @@ if (single_character == five_character_word[3]):
     print(single_character + " found at index 3")
 if (single_character == five_character_word[4]):
     print(single_character + " found at index 4")
+
+counting_variable: int = 0
+
+if (single_character == five_character_word[0]):
+    counting_variable = counting_variable + 1
+if (single_character == five_character_word[1]):
+    counting_variable = counting_variable + 1
+if (single_character == five_character_word[2]):
+    counting_variable = counting_variable + 1
+if (single_character == five_character_word[3]):
+    counting_variable = counting_variable + 1
+if (single_character == five_character_word[4]):
+    counting_variable = counting_variable + 1
