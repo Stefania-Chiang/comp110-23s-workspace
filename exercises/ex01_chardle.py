81586f88c55cad440dbb903021c7dcac453bf60d
Stefania Chiang <tinge@ad.unc.edu>
2023-01-31 14:42:52 -0500

    w

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -15,28 +15,22 @@ if len(single_character_word) != 1:
 
 print("Searching for " + single_character_word + " in " + five_character_word)
 
-if (single_character_word == five_character_word[0]):
-    print(single_character_word + " found at index 0")
-if (single_character_word == five_character_word[1]):
-    print(single_character_word + " found at index 1")
-if (single_character_word == five_character_word[2]):
-    print(single_character_word + " found at index 2")
-if (single_character_word == five_character_word[3]):
-    print(single_character_word + " found at index 3")
-if (single_character_word == five_character_word[4]):
-    print(single_character_word + " found at index 4")
-
 counting_variable: int = 0
 
 if (single_character_word == five_character_word[0]):
+    print(single_character_word + " found at index 0")
     counting_variable = counting_variable + 1
 if (single_character_word == five_character_word[1]):
+    print(single_character_word + " found at index 1")
     counting_variable = counting_variable + 1
 if (single_character_word == five_character_word[2]):
+    print(single_character_word + " found at index 2")
     counting_variable = counting_variable + 1
 if (single_character_word == five_character_word[3]):
+    print(single_character_word + " found at index 3")
     counting_variable = counting_variable + 1
 if (single_character_word == five_character_word[4]):
+    print(single_character_word + " found at index 4")
     counting_variable = counting_variable + 1
 
 if counting_variable == 0:
