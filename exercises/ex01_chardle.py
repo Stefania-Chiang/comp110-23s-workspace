9f563437e69b1fe58ec33d6c7af39250f1e501d7
Stefania Chiang <tinge@ad.unc.edu>
2023-01-31 01:18:31 -0500

    e

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -1,12 +1,13 @@
 """EX01 - Chardle - A cute step toward Wordle."""
 __author__ = "730517776"
 
-five_character_word: str = input('Enter a 5-character word: ')
+five_character_word: str = input("Enter a 5-character word: ")
+
 if len(five_character_word) != 5:
     print("Error: Word must contain 5 characters")
     exit()
 
-single_character: str = input('Enter a single character: ')
+single_character: str = input("Enter a single character: ")
 
 if len(single_character) != 1:
     print("Error: Character must be a single character.")
