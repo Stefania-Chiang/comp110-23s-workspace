ee2ced9e5cb0e73da83ea02203831feaa1e3d09a
Stefania Chiang <tinge@ad.unc.edu>
2023-01-31 00:18:19 -0500

    y

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -6,5 +6,16 @@ single_character: str = input('Enter a single character: ')
 print("Searching for " + single_character + " in " + five_character_word)
 
 if (single_character == "five_character_word"[0]):
-    print(single_character + " found at index 0" )
-
+    print(single_character + " found at index 0")
+else:
+    if (single_character == "five_character_word"[1]):
+        print(single_character + " found at index 1")
+    else:
+        if (single_character == "five_character_word"[2]):
+            print(single_character + " found at index 2")
+        else:
+            if (single_character == "five_character_word"[3]):
+                print(single_character + " found at index 3")
+            else:
+                if (single_character == "five_character_word"[4]):
+                    print(single_character + " found at index 4")
