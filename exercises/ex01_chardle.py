7844ded86ec3bc931883a95ecec7fa8d8798131e
Stefania Chiang <tinge@ad.unc.edu>
2023-01-30 21:19:21 -0500

    e

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -3,4 +3,3 @@ __author__ = "730517776"
 
 five_character_word: str = input('Enter a 5-character word: ')
 single_character: str = input('Enter a single character:')
-print("Searching for" +single_character " in hello")
