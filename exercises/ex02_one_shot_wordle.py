f05aa4a512f81413e26087f8afd25da43c8636be
Stefania Chiang <tinge@ad.unc.edu>
2023-02-08 16:33:31 -0500

    h

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -15,4 +15,13 @@ while playing:
             playing = False
         if len(guess) != 6:
                 print(f"That was not 6 letters! Try again: ")
+                playing = True
+        if len(guess) != 6:
+                print(f"That was not 6 letters! Try again: ")
+                playing = True
+        if len(guess) != 6:
+                print(f"That was not 6 letters! Try again: ")
+                playing = True
+        if len(guess) != 6:
+                print(f"That was not 6 letters! Try again: ")
                 playing = False
