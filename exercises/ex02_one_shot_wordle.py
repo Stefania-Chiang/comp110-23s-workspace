8f05493b95689424cd2759f11ec6b437d4c07ca1
Stefania Chiang <tinge@ad.unc.edu>
2023-02-08 01:06:41 -0500

    t

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,8 +1,19 @@
 """EX02 - One-Shot Wordle - Loops!"""
 __author__ = "730517776"
 
-six_letter_guess: str = input("What is your 6-letter guess? ")
+SECRET: str = str("python")
+guess: str = str(input("What is your 6-letter guess? "))
+playing: bool = True
 
-if len(six_letter_guess) != 6:
-    print(" That was not 6 letters! Try again:")
-    exit()
+while playing:
+    if len(guess) != 6:
+        print(" That was not 6 letters! Try again: ")
+    if len(guess) != 6:
+        print(" That was not 6 letters! Try again: ")
+    if len(guess) != 6:
+        print(" That was not 6 letters! Try again: ")
+    if len(guess) != 6:
+        print(" That was not 6 letters! Try again: ")
+    if len(guess) != 6:
+        print(" Not quite. Play again soon! ")
+        exit()
