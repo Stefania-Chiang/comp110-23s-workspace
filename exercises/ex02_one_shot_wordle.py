397d67751e682e26dea6e712e7be07e5f00e6ed8
Stefania Chiang <tinge@ad.unc.edu>
2023-02-08 19:23:49 -0500

    g

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -4,6 +4,36 @@ __author__ = "730517776"
 SECRET: str = "python"
 guess: str = input("What is your 6-letter guess? ")
 playing: bool = True
+WHITE_BOX: str = "\U00002B1C"
+GREEN_BOX: str = "\U0001F7E9"
+YELLOW_BOX: str = "\U0001F7E8"
+
+while playing:
+    if guess[0] == SECRET[0]:
+        print(GREEN_BOX)
+    else:
+        print(WHITE_BOX)
+    if guess[1] == SECRET[1]:
+        print(GREEN_BOX)
+    else:
+        print(WHITE_BOX)
+    if guess[2] == SECRET[2]:
+        print(GREEN_BOX)
+    else:
+        print(WHITE_BOX)
+    if guess[3] == SECRET[3]:
+        print(GREEN_BOX)
+    else:
+        print(WHITE_BOX)
+    if guess[4] == SECRET[4]:
+        print(GREEN_BOX)
+    else:
+        print(WHITE_BOX)
+    if guess[5] == SECRET[5]:
+        print(GREEN_BOX)
+    else:
+        print(WHITE_BOX)
+        playing = False
 
 while playing:
     if guess == SECRET:
@@ -15,8 +45,3 @@ while playing:
             playing = False
         else:
             guess = input("That was not 6 letters! Try again: ")
-
-WHITE_BOX: str = "\U00002B1C"
-GREEN_BOX: str = "\U0001F7E9"
-YELLOW_BOX: str = "\U0001F7E8"
-
