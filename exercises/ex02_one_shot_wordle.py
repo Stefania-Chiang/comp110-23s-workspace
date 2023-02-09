e48775834544a34a58cfd68068bcb0659433f71c
Stefania Chiang <tinge@ad.unc.edu>
2023-02-08 20:06:34 -0500

    t

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -20,3 +20,14 @@ while playing:
             playing = False
         else:
             guess = input("That was not 6 letters! Try again: ")
+
+while playing:
+    if len(guess) == len(SECRET):
+        if guess == SECRET:
+            print(f"Woo! You got it! ")
+            playing = False
+        else:
+            print(f"Not quite. Play again soon! ")
+            playing = False
+    else:
+        guess = input("That was not 6 letters! Try again: ")
