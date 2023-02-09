7fbd2d07e53f0baf818ac5b5f7db9a59b9f6eb58
Stefania Chiang <tinge@ad.unc.edu>
2023-02-08 19:07:12 -0500

    rt

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,6 +1,10 @@
 """EX02 - One-Shot Wordle - Loops!"""
 __author__ = "730517776"
 
+WHITE_BOX: str = "\U00002B1C"
+GREEN_BOX: str = "\U0001F7E9"
+YELLOW_BOX: str = "\U0001F7E8"
+
 SECRET: str = "python"
 guess: str = input("What is your 6-letter guess? ")
 playing: bool = True
