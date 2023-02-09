8c3239c2f2735293e99da747f981261d1c1fbf7b
Stefania Chiang <tinge@ad.unc.edu>
2023-02-09 12:28:37 -0500

    h

--- drafts/ex02_testing_2.py
+++ drafts/ex02_testing_2.py
@@ -2,14 +2,9 @@ SECRET: str = "python"
 guess: str = input("What is your 6-letter guess? ")
 playing: bool = True
 
-WHITE_BOX: str = "\U00002B1C"
-GREEN_BOX: str = "\U0001F7E9"
-YELLOW_BOX: str = "\U0001F7E8"
-
 while playing:
     if len(guess) == len(SECRET):
         if guess == SECRET:
-            print(GREEN_BOX+GREEN_BOX+GREEN_BOX+GREEN_BOX+GREEN_BOX+GREEN_BOX)
             print("Woo! You got it! ")
             playing = False
         else:
