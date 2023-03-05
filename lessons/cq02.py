d45cda68f713e26ddc1a44899c926e0eaeacc50e
Stefania Chiang <tinge@ad.unc.edu>
2023-03-05 15:54:26 -0500

    e

---
+++ lessons/cq02.py
@@ -0,0 +1,9 @@
+"""Practicing for ... in ... loops."""
+
+def sum(xs: list[float]) -> float:
+    """return sum off all elements in xs"""
+    sum_old: float = 0.0
+    idx: int = 0
+
+    for sum_total in xs:
+        return sum_total
