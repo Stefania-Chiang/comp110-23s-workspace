6e2c8764e3ebd68a30bd077ecda374dc59da78c5
Stefania Chiang <tinge@ad.unc.edu>
2023-03-05 23:16:21 -0500

    w

--- lessons/sum.py
+++ lessons/sum.py
@@ -2,8 +2,10 @@
 
 def sum(xs: list[float]) -> float:
     """return sum off all elements in xs"""
+    sum_total: float = 0.0
     idx: int = 0
     for idx in range(0, len(xs)):
+        sum_total += xs[idx]
         idx += 1
-        return idx
+    return sum_total
     
