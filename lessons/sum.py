79ed266c190c21b4eff6c2db27021b96f9dfe549
Stefania Chiang <tinge@ad.unc.edu>
2023-03-05 21:29:01 -0500

    j

--- lessons/sum.py
+++ lessons/sum.py
@@ -1,4 +1,4 @@
-"""Practicing for ... in ... loops."""
+"""Example function for unit tests"""
 
 def sum(xs: list[float]) -> float:
     """return sum off all elements in xs"""
@@ -6,6 +6,6 @@ def sum(xs: list[float]) -> float:
     idx: int = 0
 
     for sum_total in xs:
+        sum_total += xs[idx]
+        idx += 1
         return sum_total
-    sum_total += xs[idx]
-    idx += 1
