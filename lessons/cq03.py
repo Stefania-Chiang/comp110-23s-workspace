32da99e55f14fbe0e4c5beb2ebb68a89bb0b60a9
Stefania Chiang <tinge@ad.unc.edu>
2023-03-06 00:15:19 -0500

    w

---
+++ lessons/cq03.py
@@ -0,0 +1,9 @@
+"""Example function for unit tests"""
+
+def sum(xs: list[float]) -> float:
+    """return sum off all elements in xs"""
+    sum_total: float = 0.0
+    idx: int = 0
+    for idx in range(0, len(xs)):
+        sum_total += xs[idx]
+    return sum_total
