7fac52081cc2831f08b73e1ef73fb08dde3a34e5
Stefania Chiang <tinge@ad.unc.edu>
2023-03-08 18:13:30 -0500

    g

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,6 +1,9 @@
 """EX05 - List Utility Functions."""
 __author__ = "730517776"
 
+
 def only_evens(input_list: list[int]) -> list[int]:
     """Return a new list containing only the elements of the input list that were even"""
-
+    for num in input_list:
+        if num % 2 == 0:
+            print(num)
