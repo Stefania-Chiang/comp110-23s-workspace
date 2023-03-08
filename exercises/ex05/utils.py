10788187d1edfd416814866b16326bb48a013592
Stefania Chiang <tinge@ad.unc.edu>
2023-03-08 18:30:04 -0500

    g

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -4,6 +4,12 @@ __author__ = "730517776"
 
 def only_evens(input_list: list[int]) -> list[int]:
     """Return a new list containing only the elements of the input list that were even"""
-    for num in input_list:
-        if num % 2 == 0:
-            print(num)
+    new_list: list[int]
+    idx: int = 0
+    while idx < len(input_list):
+        for num in input_list:
+            if num % 2 == 0:
+                return num
+            new_list.append(num)
+            idx += 1
+    return new_list
