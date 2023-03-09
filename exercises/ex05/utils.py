c4cfb0d8d8cfb8621e109d2dccfe3cf552be8c4e
Stefania Chiang <tinge@ad.unc.edu>
2023-03-08 20:04:55 -0500

    f

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -23,5 +23,5 @@ def concat(first_list: list[int], second_list: list[int]) -> list[int]:
 
 def sub(a_list: list[int], start_index: int, end_index: int) ->list[int]:
     """Generate a list which is a subset of the given list, between the specified start index and the end index - 1."""
-    new_list: list[int]
-    
+    new_list: list[int] = [a_list[start_index], a_list[end_index - 1]]
+    return new_list
