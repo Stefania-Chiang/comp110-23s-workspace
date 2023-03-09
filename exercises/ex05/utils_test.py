259c987a0eff56387f1c4934a1fbcbe7d1d226ab
Stefania Chiang <tinge@ad.unc.edu>
2023-03-08 22:00:02 -0500

    g

--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -3,9 +3,6 @@ __author__ = "730517776"
 
 from exercises.ex05.utils import only_evens
 
-def test_empty() -> None:
-    assert only_evens([]) == []
-
 def test_one_num() -> None:
     test_list: list[int] = [2]
     assert only_evens(test_list) == [2]
@@ -13,4 +10,3 @@ def test_one_num() -> None:
 def test_many_num() -> None:
     test_list: list[int] = [1, 2, 3, 4, 5, 6]
     assert only_evens(test_list) == [2, 4, 6]
-
