55f060576ff153254bcb90b1340561096d2684ec
Stefania Chiang <tinge@ad.unc.edu>
2023-03-08 21:51:23 -0500

    w

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -5,21 +5,16 @@ __author__ = "730517776"
 def only_evens(input_list: list[int]) -> list[int]:
     """Return a new list containing only the elements of the input list that were even."""
     for num in input_list:
-        new_list: list[int]
         if num % 2 == 0:
-            new_list.append(num)
-    return new_list
+            input_list.append(num)
+    return input_list
 
 
 def concat(first_list: list[int], second_list: list[int]) -> list[int]:
     """Generate a new List which contains all of the elements of the first list followed by all of the elements of the second list."""
-    for num in first_list:
-        new_list: list[int]
-        new_list.append(num)
     for num in second_list:
-        new_list: list[int]
-        new_list.append(num)
-    return new_list
+        first_list.append(num)
+    return first_list
 
 
 def sub(a_list: list[int], start_index: int, end_index: int) -> list[int]:
