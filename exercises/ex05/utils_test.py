ce0dd853192f49d4a7df527c685265ce73fa9d3c
Stefania Chiang <tinge@ad.unc.edu>
2023-03-08 21:27:13 -0500

    g

--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,3 +1,16 @@
 """Unit tests for list utility functions."""
 __author__ = "730517776"
 
+from exercises.ex05.utils import only_evens
+
+def test_empty() -> None:
+    assert only_evens([]) == []
+
+def test_one_num() -> None:
+    test_list: list[int] = [2]
+    assert only_evens(test_list) == [2]
+
+def test_many_num() -> None:
+    test_list: list[int] = [1, 2, 3, 4, 5, 6]
+    assert only_evens(test_list) == [2, 4, 6]
+
