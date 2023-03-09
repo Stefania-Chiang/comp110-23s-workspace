cb4eab493945d9cdecade1f6a71fb0ae79963dc3
Stefania Chiang <tinge@ad.unc.edu>
2023-03-09 00:03:48 -0500

    h

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -7,17 +7,17 @@ def only_evens(input_list: list[int]) -> list[int]:
     for num in input_list:
         if num % 2 == 0:
             input_list.append(num)
-    print(input_list)
+    return input_list
 
 
 def concat(first_list: list[int], second_list: list[int]) -> list[int]:
     """Generate a new List which contains all of the elements of the first list followed by all of the elements of the second list."""
     for num in second_list:
         first_list.append(num)
-    print(first_list)
+    return first_list
 
 
 def sub(a_list: list[int], start_index: int, end_index: int) -> list[int]:
     """Generate a list which is a subset of the given list, between the specified start index and the end index - 1."""
     a_list: list[int] = [a_list[start_index], a_list[end_index - 1]]
-    print(a_list)
+    return a_list
