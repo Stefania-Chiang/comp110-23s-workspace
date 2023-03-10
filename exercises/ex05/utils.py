585e022a2fa536a03121a3490e1837bb98723e5a
Stefania Chiang <tinge@ad.unc.edu>
2023-03-10 15:44:12 -0500

    w

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -13,7 +13,9 @@ def only_evens(old_list: list[int]) -> list[int]:
 
 def concat(first_list: list[int], second_list: list[int]) -> list[int]:
     """Return a new list containing all the numbers of the first list and the second list."""
-    new_list: list[int] = first_list
+    new_list: list[int] = []
+    for num in first_list:
+        new_list.append(num)
     for num in second_list:
         new_list.append(num)
     return new_list
@@ -22,7 +24,7 @@ def concat(first_list: list[int], second_list: list[int]) -> list[int]:
 def sub(given_list: list[int], start_index: int, end_index: int) -> list[int]:
     """Return a new list which is subset of the given list between start index and end index - 1."""
     new_list: list[int] = []
-    if len(given_list) == 0 or start_index >= len(given_list) or end_index <= 0:
+    if len(given_list) == 0 or start_index >= len(given_list) or start_index <= 0 or end_index >= len(given_list) or end_index <= 0:
         new_list = []
         return new_list
     while start_index < end_index:
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -27,23 +27,23 @@ def test_only_evens_with_no_num() -> None:
 
 def test_concat_with_positive_num() -> None:
     """Test concat with positive numbers."""
-    test_first_list: list[int] = [9, 8 ,7]
+    test_first_list: list[int] = [9, 8, 7]
     test_second_list: list[int] = [100, 200, 300]
-    assert concat(test_first_list,  test_second_list) == [9, 8, 7, 100, 200, 300]
+    assert concat(test_first_list, test_second_list) == [9, 8, 7, 100, 200, 300]
 
 
 def test_concat_with_negative_num() -> None:
     """Test concat with negative numbers."""
     test_first_list: list[int] = [-7, -8, -9]
     test_second_list: list[int] = [-30, -20, -10]
-    assert concat(test_first_list,  test_second_list) == [-7, -8, -9, -30, -20, -10]
+    assert concat(test_first_list, test_second_list) == [-7, -8, -9, -30, -20, -10]
 
 
 def test_concat_with_no_num() -> None:
     """Test concat with no number."""
     test_first_list: list[int] = []
     test_second_list: list[int] = []
-    assert concat(test_first_list,  test_second_list) == []
+    assert concat(test_first_list, test_second_list) == []
 
 
 def test_sub_with_one_num() -> None:
@@ -56,7 +56,7 @@ def test_sub_with_one_num() -> None:
 
 def test_sub_with_many_num() -> None:
     """Test sub with many numbers."""
-    test_given_list: list[int]= [19, 17, 15, 13, 11]
+    test_given_list: list[int] = [19, 17, 15, 13, 11]
     test_start_index: int = 2
     test_end_index: int = 4
     assert sub(test_given_list, test_start_index, test_end_index) == [15, 13]
@@ -64,7 +64,7 @@ def test_sub_with_many_num() -> None:
 
 def test_sub_with_no_num() -> None:
     """Test sub with no number."""
-    test_given_list: list[int]= []
+    test_given_list: list[int] = []
     test_start_index: int = 3
     test_end_index: int = 5
     assert sub(test_given_list, test_start_index, test_end_index) == []
