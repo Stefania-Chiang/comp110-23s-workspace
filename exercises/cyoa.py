7f63e7a3421b697eb866fd1da1c2fbce7e3b6c89
Stefania Chiang <tinge@ad.unc.edu>
2023-03-13 18:00:17 -0400

    s

---
+++ exercises/cyoa.py
@@ -0,0 +1,5 @@
+"""EX06 - Choose Your Own Adventure."""
+__author__ = "730517776"
+
+if __name__ == "__main__":
+  main()
---
+++ lessons/cq04.py
@@ -0,0 +1,3 @@
+"""Challenge Question 04 - Dict Function Writing."""
+__author__ = "730517776"
+
