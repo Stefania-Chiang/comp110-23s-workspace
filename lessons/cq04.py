c051d9b32b545debf2072a9ddcc8c892206749ed
Stefania Chiang <tinge@ad.unc.edu>
2023-03-20 13:56:06 -0400

    s

--- lessons/cq04.py
+++
@@ -1,5 +0,0 @@
-"""Challenge Question 04 - Dict Function Writing."""
-__author__ = "730517776"
-
-def zip(input_intergers: list[int], input_strings: list[str]) -> dict[str, int]:
-    
---
+++ lessons/zip.py
@@ -0,0 +1,14 @@
+"""Challenge Question 04 - Dict Function Writing."""
+__author__ = "730517776"
+
+
+def zip(first_list: list[str], second_list: list[int]) -> dict[str, int]:
+    input_list: dict[str, int] = {}
+    idx: int = 0
+    if len(first_list) == len(second_list) and len(first_list) != 0:
+        while idx < len(first_list):
+            input_list["first_list[idx]"] = second_list[idx]
+            idx += 1
+        return input_list 
+    else:
+        return input_list
