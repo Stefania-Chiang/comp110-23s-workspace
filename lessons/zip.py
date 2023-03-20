d73ef8eedd3beea3c82ac3393cc66fef32044ad7
Stefania Chiang <tinge@ad.unc.edu>
2023-03-20 17:33:25 -0400

    w

--- lessons/zip.py
+++ lessons/zip.py
@@ -7,7 +7,7 @@ def zip(first_list: list[str], second_list: list[int]) -> dict[str, int]:
     idx: int = 0
     if len(first_list) == len(second_list) and len(first_list) != 0:
         while idx < len(first_list):
-            input_list["first_list[idx]"] = second_list[idx]
+            input_list[first_list[idx]] = second_list[idx]
             idx += 1
         return input_list 
     else:
