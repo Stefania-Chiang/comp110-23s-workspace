15c5421fbeed55660e75b79c8144ed8e11d893e2
Stefania Chiang <tinge@ad.unc.edu>
2023-03-21 16:25:34 -0400

    w

--- exercises/coya.py
+++ exercises/coya.py
@@ -34,50 +34,49 @@ def greet() -> None:
 
 def explore_asia() -> None:
     """Exploring Asia."""
-    global player
-    print(f"\n{player}, you have arrived in Asia.\nYou hear something in the bamboo forest, do you want to check it out?\n1. Yes\n2. No\n")
-    choice: int = input("Enter your choice (1-2):\n")
+    print(f"{player}, you have arrived in Asia.\nYou hear something in the bamboo forest, do you want to check it out?\n1. Yes\n2. No")
+    choice: int = input("Enter your choice (1-2): ")
     if choice == "1":
-        print("\nYou discover a cute panda!\nYou earn 40 adventure points.")
+        print("You discover a cute panda!\nYou earn 40 adventure points.")
         points += 40
     if choice == "2":
-        print("\nYou decide not to take the risk, and you leave the area safely.\nYou earn 20 adventure points.")
+        print("You decide not to take the risk, and you leave the area safely.\nYou earn 20 adventure points.")
         points += 20
     else:
-        print("\nSorry, your choice is unavailable. Please enter a number from 1 to 2.")
+        print("Sorry, your choice is unavailable. Please enter a number from 1 to 2.")
     return points
 
 
-def explore_europe(points: int) -> None:
+def explore_europe() -> None:
     """Exploring Europe."""
-    print(f"\n{player}, you are now in Europe.\nYou see a castle, but the gate is locked, how can you get in?\n1. Knock on the gate\n2. Look around for secret entrance\n")
-    choice = input("Enter your choice (1-2):\n")
+    print(f"{player}, you are now in Europe.\nYou see a castle, but the gate is locked, how can you get in?\n1. Knock on the gate\n2. Look around for secret entrance")
+    choice = input("Enter your choice (1-2): ")
     if choice == "1":
-        print("\nYou mistakenly enter the evil forces territory.\nYou lose 10 adventure points.")
+        print("You mistakenly enter the evil forces territory.\nYou lose 10 adventure points.")
         points -= 10
     if choice == "2":
-        print("\nYou peek into the secret entrance and discover the evil forces insdie, so you leave the area quickly.\nYou earn 30 adventure points.")
+        print("You peek into the secret entrance and discover the evil forces insdie, so you leave the area quickly.\nYou earn 30 adventure points.")
         points += 30
     else:
-        print("\nSorry, your choice is unavailable. Please enter a number from 1 to 2.")
+        print("Sorry, your choice is unavailable. Please enter a number from 1 to 2.")
     return points
 
 
 def explore_africa() -> None:
     """Exploring Africa."""
-    print(f"\n{player}, you have traveled to Africa.\nYou see a lion approaching a deer, and you have a gun in your hand, what should you do?\n1. Shoot in the air to scare them\n2. Shoot the lion\n3. Watch quietly\n")
-    choice = input("Enter your choice (1-3):\n")
+    print(f"{player}, you have traveled to Africa.\nYou see a lion approaching a deer, and you have a gun in your hand, what should you do?\n1. Shoot in the air to scare them\n2. Shoot the lion\n3. Watch quietly")
+    choice = input("Enter your choice (1-3): ")
     if choice == "1":
-        print("\nYou shouldn't interrupt the process of nature.\nYou lose 20 adventure points.")
+        print("You shouldn't interrupt the process of nature.\nYou lose 20 adventure points.")
         points -= 20
     if choice == "2":
-        print("\nYou miss your shot, so now the lion is coming for you.\nYou lose 30 adventure points.")
+        print("You miss your shot, so now the lion is coming for you.\nYou lose 30 adventure points.")
         points -= 30
     if choice == "3":
-        print("\nYou play smart and witness a cool process of nature.\nYou earn 10 adventure points.")
+        print("You play smart and witness a cool process of nature.\nYou earn 10 adventure points.")
         points += 10
     else:
-        print("\nSorry, your choice is unavailable. Please enter a number from 1 to 3.")
+        print("Sorry, your choice is unavailable. Please enter a number from 1 to 3.")
     return points
 
 
