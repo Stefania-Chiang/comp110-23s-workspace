561e1aca45dd098a7152e1852988c6a664490fcd
Stefania Chiang <tinge@ad.unc.edu>
2023-03-20 20:46:54 -0400

    w

--- exercises/cyoa.py
+++ exercises/cyoa.py
@@ -1,63 +1,90 @@
 """EX06 - Choose Your Own Adventure."""
 __author__ = "730517776"
 
-# Required Global Variables
-points = 0
-player = ""
+import random
 
-# Greet Procedure
-def greet():
-    global player
-    print("Welcome to the Choose Your Own Adventure game!")
-    player = input("What is your name?\n")
-    print(f"\nHi {player}! Let's begin your adventure.\n")
-
-# Custom Procedure
-def path_one():
-    global points
-    print(f"\n{player}, you have chosen path one!")
-    print("You find yourself in a dark cave. You see a torch and a sword.")
-    choice = input("Do you take the torch or the sword? (torch/sword)\n")
-    if choice == "torch":
-        print("You take the torch and move deeper into the cave.")
-        points += 10
-    elif choice == "sword":
-        print("You take the sword and move deeper into the cave.")
-        points += 20
-    else:
-        print("Invalid choice. Please choose again.")
-        path_one()
-
-# Custom Function
-def path_two(points):
-    print(f"\n{player}, you have chosen path two!")
-    print("You find yourself on a mountaintop with a breathtaking view.")
-    choice = input("Do you want to take a picture or go paragliding? (picture/paragliding)\n")
-    if choice == "picture":
-        print("You take a picture and enjoy the view.")
-        points += 5
-    elif choice == "paragliding":
-        print("You go paragliding and have the time of your life!")
-        points += 30
-    else:
-        print("Invalid choice. Please choose again.")
-        path_two(points)
+# Global Variables
+POINTS = 0
+PLAYER = ""
 
 # Main Function
-def main():
-    greet()
-    print("You find yourself at a crossroads. Which path will you take?")
-    while True:
-        choice = input("Enter 1 to take path one, 2 to take path two, or 3 to end the game.\n")
-        if choice == "1":
-            path_one()
-        elif choice == "2":
-            points = path_two(points)
-        elif choice == "3":
-            print(f"\nGoodbye, {player}! You accumulated {points} adventure points.")
-            return None
-        else:
-            print("Invalid choice. Please choose again.")
+def main() -> None:
+  """Main adventure game loop."""
+  greet()
+  while True:
+    print("Which continent do you want to explore?\n")
+    print("1. Asia\n")
+    print("2. Europe\n")
+    print("3. Africa\n")
+    print("4. End my adventure\n")
+    choice = input("Enter your choice (1-4): ")
+    if choice == "1":
+      explore_asia()
+    if choice == "2":
+      explore_europe()
+    if choice == "3":
+      explore_africa()
+    if choice == "4":
+      print(f"\nGoodbye {PLAYER}, congratulations for earning {POINTS} points during your journey!")
+      return None
+    else:
+      print("Sorry, your choice is unavailable. Please enter a number from 1 to 4.")
 
-if __name__ == "__main__":
-    main()
+# Greet Procedure
+def greet() -> None:
+  print("Welcome to the Around the World travel game!\n")
+  PLAYER = input("What is the passenger's name?\n")
+  print(f"\nHello {PLAYER}, let's begin your journey!\n")
+
+# Custom Procedure: explore the forest
+def explore_asia() -> None:
+    global POINTS
+    print(f"\n {PLAYER}, you are now in the dark forest.")
+    print("You hear rustling in the bushes, do you investigate?")
+    print("1. Yes")
+    print("2. No")
+    choice = input("Enter your choice (1-2): ")
+    if choice == "1":
+        print("You investigate and find a lost treasure! You earn 10 adventure points.")
+        POINTS += 10
+    elif choice == "2":
+        print("You decide to play it safe and leave the area.")
+    else:
+        print("Invalid choice, please enter a number from 1-2.")
+
+# Custom function: enter the castle
+def explore_europe(points) -> None:
+    print(f"\n{PLAYER}, you are now at the entrance of the abandoned castle.")
+    print("You see a locked door, how do you open it?")
+    print("1. Pick the lock")
+    print("2. Look for the key")
+    choice = input("Enter your choice (1-2): ")
+    if choice == "1":
+        print("You attempt to pick the lock, but fail. You lose 5 adventure points.")
+        points -= 5
+    elif choice == "2":
+        print("You search the area and find the key! You earn 20 adventure points.")
+        points += 20
+    else:
+        print("Invalid choice, please enter a number from 1-2.")
+    return points
+
+# Custom procedure: go back to town
+def explore_africa():
+    global POINTS
+    print(f"\n{PLAYER}, you are now back in town.")
+    print("You see a group of bandits causing trouble, what do you do?")
+    print("1. Confront the bandits")
+    print("2. Alert the authorities")
+    print("3. Ignore them and go about your business")
+    choice = input("Enter your choice (1-3): ")
+    if choice == "1":
+        print("You take on the bandits and win! You earn 15 adventure points.")
+        POINTS += 15
+    elif choice == "2":
+        print("You report the bandits to the authorities and they are apprehended. You earn 5 adventure points.")
+        POINTS += 5
+    elif choice == "3":
+        print("You decide to avoid the bandits and go about your business.")
+    else:
+        print("Invalid choice, please enter a number")
---
+++ exercises/ex06.py
@@ -0,0 +1,67 @@
+"""EX06 - Choose Your Own Adventure."""
+__author__ = "730517776"
+
+# 0.0 Establish a global variable named points to track “adventure points”.
+points: int = 0
+
+# 0.1 Establish a global variable named player to track the player’s name.
+player: str = ""
+
+# 1.0 Define a main function, that returns None, and follows the Python idiom for calling main at the end of the program.
+
+# Greet Procedure
+def greet():
+    global player
+    print("Welcome to the Choose Your Own Adventure game!")
+    player = input("What is your name?\n")
+    print(f"\nHi {player}! Let's begin your adventure.\n")
+
+# Custom Procedure
+def path_one():
+    global points
+    print(f"\n{player}, you have chosen path one!")
+    print("You find yourself in a dark cave. You see a torch and a sword.")
+    choice = input("Do you take the torch or the sword? (torch/sword)\n")
+    if choice == "torch":
+        print("You take the torch and move deeper into the cave.")
+        points += 10
+    elif choice == "sword":
+        print("You take the sword and move deeper into the cave.")
+        points += 20
+    else:
+        print("Invalid choice. Please choose again.")
+        path_one()
+
+# Custom Function
+def path_two(points):
+    print(f"\n{player}, you have chosen path two!")
+    print("You find yourself on a mountaintop with a breathtaking view.")
+    choice = input("Do you want to take a picture or go paragliding? (picture/paragliding)\n")
+    if choice == "picture":
+        print("You take a picture and enjoy the view.")
+        points += 5
+    elif choice == "paragliding":
+        print("You go paragliding and have the time of your life!")
+        points += 30
+    else:
+        print("Invalid choice. Please choose again.")
+        path_two(points)
+
+# Main Function
+def main():
+    greet()
+    print("You find yourself at a crossroads. Which path will you take?")
+    while True:
+        choice = input("Enter 1 to take path one, 2 to take path two, or 3 to end the game.\n")
+        if choice == "1":
+            path_one()
+        elif choice == "2":
+            points = path_two(points)
+        elif choice == "3":
+            print(f"\nGoodbye, {player}! You accumulated {points} adventure points.")
+            return None
+        else:
+            print("Invalid choice. Please choose again.")
+
+if __name__ == "__main__":
+    main()
