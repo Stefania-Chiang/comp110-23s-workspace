0aee5c66a90ef8063c2f78f9bbfa0f597b515318
Stefania Chiang <tinge@ad.unc.edu>
2023-03-21 17:18:05 -0400

    g

--- exercises/cyoa.py
+++ exercises/cyoa.py
@@ -8,6 +8,8 @@ points: int = 0
 
 def main() -> None:
     """Main function."""
+    global player
+    global points
     greet()
     playing: bool = True
     while playing:
@@ -16,7 +18,7 @@ def main() -> None:
         if choice == "1":
             explore_asia()
         if choice == "2":
-            points = explore_europe(points)
+            points = explore_europe()
         if choice == "3":
             explore_africa()
         if choice == "4":
@@ -27,6 +29,7 @@ def main() -> None:
 
 def greet() -> None:
     """Greet procedure and welcome message."""
+    global player
     print("Welcome to the Around the World travel game!")
     player: str = input("What is the passenger's name? ")
     print(f"Hello {player}, let's begin your journey!")
@@ -34,21 +37,24 @@ def greet() -> None:
 
 def explore_asia() -> None:
     """Exploring Asia."""
+    global player
+    global points
     print(f"{player}, you have arrived in Asia.\nYou hear something in the bamboo forest, do you want to check it out?\n1. Yes\n2. No")
     choice: int = input("Enter your choice (1-2): ")
     if choice == "1":
         print("You discover a cute panda!\nYou earn 40 adventure points.")
         points += 40
     if choice == "2":
-        print("You decide not to take the risk, and you leave the area safely.\nYou earn 20 adventure points.")
+        print("You decide not to take the risk, and you leave the area s.afely.\nYou earn 20 adventure points.")
         points += 20
     else:
         print("Sorry, your choice is unavailable. Please enter a number from 1 to 2.")
-    return points
 
 
 def explore_europe() -> None:
     """Exploring Europe."""
+    global player
+    global points
     print(f"{player}, you are now in Europe.\nYou see a castle, but the gate is locked, how can you get in?\n1. Knock on the gate\n2. Look around for secret entrance")
     choice = input("Enter your choice (1-2): ")
     if choice == "1":
@@ -59,11 +65,12 @@ def explore_europe() -> None:
         points += 30
     else:
         print("Sorry, your choice is unavailable. Please enter a number from 1 to 2.")
-    return points
 
 
 def explore_africa() -> None:
     """Exploring Africa."""
+    global player
+    global points
     print(f"{player}, you have traveled to Africa.\nYou see a lion approaching a deer, and you have a gun in your hand, what should you do?\n1. Shoot in the air to scare them\n2. Shoot the lion\n3. Watch quietly")
     choice = input("Enter your choice (1-3): ")
     if choice == "1":
@@ -77,7 +84,6 @@ def explore_africa() -> None:
         points += 10
     else:
         print("Sorry, your choice is unavailable. Please enter a number from 1 to 3.")
-    return points
 
 
 if __name__ == "__main__":
