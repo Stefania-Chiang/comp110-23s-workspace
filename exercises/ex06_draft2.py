cf58ffdd71846d9960d0c7da9823858f3e39958f
Stefania Chiang <tinge@ad.unc.edu>
2023-03-21 15:24:23 -0400

    w

--- exercises/ex06_draft2.py
+++ exercises/ex06_draft2.py
@@ -5,15 +5,16 @@ __author__ = "730517776"
 player: str = ""
 points: int = 0
 
+
 def main() -> None:
-	"""Main function."""
-	greet()
-	playing: bool = True
-	while playing:
-		print("Which continent do you want to explore?\n1. Asia\n2. Europe\n3. Africa\n4. End my adventure\n")
-		choice: int = input("Enter your choice (1-4):\n")
-		if choice == "1":
-			explore_asia()
+    """Main function."""
+    greet()
+    playing: bool = True
+    while playing:
+	    print("Which continent do you want to explore?\n1. Asia\n2. Europe\n3. Africa\n4. End my adventure\n")
+	    choice: int = input("Enter your choice (1-4):\n")
+	    if choice == "1":
+	    explore_asia()
 		if choice == "2":
 			points = explore_europe(points)
 		if choice == "3":
