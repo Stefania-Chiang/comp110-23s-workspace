272f50416f8d4fba2c8853130560a81c28a26b5f
Stefania Chiang <tinge@ad.unc.edu>
2023-03-22 11:49:54 -0400

    w

--- exercises/cyoa.py
+++ exercises/cyoa.py
@@ -45,7 +45,7 @@ def explore_asia() -> None:
         print(f"{player}, you have arrived in Asia.\nYou hear something in the bamboo forest, do you want to check it out?\n1. Yes\n2. No")
         choice: str = input("Enter your choice (1-2): ")
         if choice != "1" and choice != "2":
-            choice = input("Sorry, your choice is unavailable in asia. Please enter a number from 1 to 2.")
+            choice = input("Sorry, your choice is unavailable in asia. Please enter a number from 1 to 2: ")
         if choice == "1":
             print("You discover a cute panda!\nYou earn 40 adventure points.")
             points += 40
@@ -65,7 +65,7 @@ def explore_europe() -> None:
         print(f"{player}, you are now in Europe.\nYou see a castle, but the gate is locked, how can you get in?\n1. Knock on the gate\n2. Look around for secret entrance")
         choice: str = input("Enter your choice (1-2): ")
         if choice != "1" and choice != "2":
-            choice = input("Sorry, your choice is unavailable in europe. Please enter a number from 1 to 2.")
+            choice = input("Sorry, your choice is unavailable in europe. Please enter a number from 1 to 2: ")
         if choice == "1":
             print("You mistakenly enter the evil forces territory.\nYou lose 10 adventure points.")
             points -= 10
@@ -85,7 +85,7 @@ def explore_africa() -> None:
         print(f"{player}, you have traveled to Africa.\nYou see a lion approaching a deer, and you have a gun in your hand, what should you do?\n1. Shoot in the air to scare them\n2. Shoot the lion\n3. Watch quietly")
         choice: str = input("Enter your choice (1-3): ")
         if choice != "1" and choice != "2" and choice != "3":
-            choice = input("Sorry, your choice is unavailable in africa. Please enter a number from 1 to 2.")
+            choice = input("Sorry, your choice is unavailable in africa. Please enter a number from 1 to 2: ")
         if choice == "1":
             print("You shouldn't interrupt the process of nature.\nYou lose 20 adventure points.")
             points -= 20
