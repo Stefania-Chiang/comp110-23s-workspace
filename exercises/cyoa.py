4032b49bb77f4134cea76987e66f7758b53421ce
Stefania Chiang <tinge@ad.unc.edu>
2023-03-22 00:45:38 -0400

    f

--- exercises/cyoa.py
+++ exercises/cyoa.py
@@ -44,7 +44,7 @@ def explore_asia() -> None:
     while playing:
         print(f"{player}, you have arrived in Asia.\nYou hear something in the bamboo forest, do you want to check it out?\n1. Yes\n2. No")
         choice: str = input("Enter your choice (1-2): ")
-        if choice != "1" or choice != "2":
+        if choice != "1" and choice != "2":
             print("Sorry, your choice is unavailable in asia. Please enter a number from 1 to 2.")
         if choice == "1":
             print("You discover a cute panda!\nYou earn 40 adventure points.")
@@ -64,7 +64,7 @@ def explore_europe() -> None:
     while playing:
         print(f"{player}, you are now in Europe.\nYou see a castle, but the gate is locked, how can you get in?\n1. Knock on the gate\n2. Look around for secret entrance")
         choice: str = input("Enter your choice (1-2): ")
-        if choice != "1" and "2":
+        if choice != "1" and choice != "2":
             print("Sorry, your choice is unavailable in europe. Please enter a number from 1 to 2.")
         if choice == "1":
             print("You mistakenly enter the evil forces territory.\nYou lose 10 adventure points.")
@@ -84,7 +84,7 @@ def explore_africa() -> None:
     while playing:
         print(f"{player}, you have traveled to Africa.\nYou see a lion approaching a deer, and you have a gun in your hand, what should you do?\n1. Shoot in the air to scare them\n2. Shoot the lion\n3. Watch quietly")
         choice: str = input("Enter your choice (1-3): ")
-        if choice != "1" and "2" and "3":
+        if choice != "1" and choice != "2" and choice != "3":
             print("Sorry, your choice is unavailable in africa. Please enter a number from 1 to 2.")
         if choice == "1":
             print("You shouldn't interrupt the process of nature.\nYou lose 20 adventure points.")
