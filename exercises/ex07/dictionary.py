433d24a425083fe4199be8dfd0b37a53a316728e
Stefania Chiang <tinge@ad.unc.edu>
2023-03-30 18:51:45 -0400

    d

--- exercises/ex07/dictionary.py
+++ exercises/ex07/dictionary.py
@@ -1,3 +1,21 @@
 """EX07 - Dictionary Functions."""
 __author__ = "730517776"
 
+
+def invert(old_dict: dict[str, str]) -> dict[str, str]:
+    """Return a new dictionary that inverts the keys and the values of the old dictionary."""
+    new_dict: dict[str, str] = {}
+    for key in old_dict:
+        new_dict[value] = old_dict[key]
+    for value in old_dict:
+        new_dict[key] = old_dict[value]
+    return new_dict
+
+
+def merge(a: dict[str, str], b: dict[str, str]) -> dict[str, str]:
+    result: dict[str, str] = {}
+    for key in a:
+        result[key] = a[key]
+    for key in b:
+        result[key] = b[key]
+    return result
