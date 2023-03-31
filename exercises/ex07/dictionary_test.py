2100b7ec9b73051eaefa407675d43d9d3d705430
Stefania Chiang <tinge@ad.unc.edu>
2023-03-31 15:27:39 -0400

    w

--- exercises/ex07/dictionary_test.py
+++ exercises/ex07/dictionary_test.py
@@ -10,3 +10,10 @@ def test_invert_with_positive_numbers() -> None:
     """Test invert with positive numbers."""
     test_old_dict: dict[str, str] = {"chocolate": "12", "vanilla": "8", "strawberry": "5"}
     assert invert(test_old_dict) == {"12": "chocolate", "8": "vanilla", "5": "strawberry"}
+
+
+def test_count_with_positive_numbers() -> None:
+    """Test count with positive numbers."""
+    test_old_list: list[str] = {"chocolate", "vanilla", "strawberry", "vanilla", "chocolate"}
+    assert count(test_old_list) == {"chocolate": 2, "vanilla": 2, "strawberry": 1}
+    
